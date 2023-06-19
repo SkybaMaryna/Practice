12f27faec540babe39f76a6775a8fb3e1afcf9c8
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-06-19 16:02:36 +0300

    practice

--- js/practice.js
+++ js/practice.js
@@ -661,12 +661,317 @@
 
 // ________________________________________________
 
-function chunkArrayInGroups(arr, size) {
-let newArr = []
-  for (let i = 0; i < arr.length; i += size){
-    newArr.push(arr.slice(i, i + size))
-  }
-  return newArr
-}
+// function chunkArrayInGroups(arr, size) {
+// let newArr = []
+//   for (let i = 0; i < arr.length; i += size){
+//     newArr.push(arr.slice(i, i + size))
+//   }
+//   return newArr
+// }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+// ________________________________________________
+
+// Глобальна змінна
+// const watchList = [
+//   {
+//     "Title": "Inception",
+//     "Year": "2010",
+//     "Rated": "PG-13",
+//     "Released": "16 Jul 2010",
+//     "Runtime": "148 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Christopher Nolan",
+//     "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//     "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//     "Language": "English, Japanese, French",
+//     "Country": "USA, UK",
+//     "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.8",
+//     "imdbVotes": "1,446,708",
+//     "imdbID": "tt1375666",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Interstellar",
+//     "Year": "2014",
+//     "Rated": "PG-13",
+//     "Released": "07 Nov 2014",
+//     "Runtime": "169 min",
+//     "Genre": "Adventure, Drama, Sci-Fi",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan, Christopher Nolan",
+//     "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//     "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//     "Language": "English",
+//     "Country": "USA, UK",
+//     "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.6",
+//     "imdbVotes": "910,366",
+//     "imdbID": "tt0816692",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "The Dark Knight",
+//     "Year": "2008",
+//     "Rated": "PG-13",
+//     "Released": "18 Jul 2008",
+//     "Runtime": "152 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//     "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//     "Language": "English, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//     "Metascore": "82",
+//     "imdbRating": "9.0",
+//     "imdbVotes": "1,652,832",
+//     "imdbID": "tt0468569",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Batman Begins",
+//     "Year": "2005",
+//     "Rated": "PG-13",
+//     "Released": "15 Jun 2005",
+//     "Runtime": "140 min",
+//     "Genre": "Action, Adventure",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//     "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//     "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//     "Language": "English, Urdu, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//     "Metascore": "70",
+//     "imdbRating": "8.3",
+//     "imdbVotes": "972,584",
+//     "imdbID": "tt0372784",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Avatar",
+//     "Year": "2009",
+//     "Rated": "PG-13",
+//     "Released": "18 Dec 2009",
+//     "Runtime": "162 min",
+//     "Genre": "Action, Adventure, Fantasy",
+//     "Director": "James Cameron",
+//     "Writer": "James Cameron",
+//     "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//     "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//     "Language": "English, Spanish",
+//     "Country": "USA, UK",
+//     "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//     "Metascore": "83",
+//     "imdbRating": "7.9",
+//     "imdbVotes": "876,575",
+//     "imdbID": "tt0499549",
+//     "Type": "movie",
+//     "Response": "True"
+//   }
+// ];
+
+// function getRating(watchList) {
+//   // Змініть код лише під цим рядком
+//    const filteredList = watchList.filter(({Director}) => Director === 'Christopher Nolan')
+//    const rating = filteredList.reduce((acc, {imdbRating}) => acc + Number(imdbRating), 0)
+//    let averageRating = rating / filteredList.length
+//   // Змініть код лише над цим рядком
+//   return averageRating;
+// }
+
+// console.log(getRating(watchList));
+
+// ________________________________________________
+
+// const squareList = arr => {
+//   // Змініть код лише під цим рядком
+//   const squareArr = arr.reduce((acc, num) => {
+//     if (num >= 0 && !num.toString().includes('.')){
+//       acc.push(num ** 2)
+//     }
+//     return acc
+//   }, [])
+//   return squareArr;
+//   // Змініть код лише над цим рядком
+// };
+//   const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+//   console.log(squaredIntegers);
+
+// ________________________________________________
+
+// function splitify(str) {
+//   // Змініть код лише під цим рядком
+// return str.split(/\W/)
+
+//   // Змініть код лише над цим рядком
+// }
+
+// console.log(splitify("Hello World,I-am code"))
+
+// ________________________________________________
+
+// Змініть код лише під цим рядком
+// function urlSlug(title) {
+//   return title
+//     .toLowerCase()
+//     .trim()
+//     .split(/\s+/)
+//     .join("-");
+// }
+// // Змініть код лише над цим рядком
+// console.log(urlSlug("A Mind Needs Books Like A Sword Needs  A Whetstone"))
+
+// ________________________________________________
+
+// function sumAll(arr) {
+//    const sortedArr = arr.sort((a,b) => a-b)
+//    let count = 0
+//    for (let i = sortedArr[0]; i <= sortedArr[sortedArr.length - 1]; i++) {
+//     count += i;
+//    }
+//     return count;
+// }
+//   console.log(sumAll([5, 10]))
+
+  // ________________________________________________
+
+  // function diffArray(arr1, arr2) {
+  //   let newArr = [];
+  //     const uniqueArr1 = arr1.filter(num => !arr2.includes(num))
+  //     const uniqueArr2 = arr2.filter(num => !arr1.includes(num))
+  //     newArr.push(...uniqueArr1, ...uniqueArr2)
+  //     return newArr;
+  // }
+  
+  // console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]))
+
+  // ________________________________________________
+
+  // function destroyer(arr) {
+  //   const [array, ...rest] = arguments;
+  //   const filteredArr = arr.filter(item => !rest.includes(item))
+  //   return filteredArr;
+  // }
+  
+  // console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
+
+  // ________________________________________________
+
+  // function whatIsInAName(collection, source) {
+  //   const sourceKeys = Object.keys(source)
+  //   return collection.filter(obj => {
+  //     for (let i = 0; i < sourceKeys.length; i++) {
+  //       if (obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+  //         return false;
+  //       }
+  //     }
+  //     return true;
+  //   });
+  // }
+  
+  // console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))
+
+  // ________________________________________________
+
+  // function spinalCase(str) {
+  //   return str.trim().split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
+  // }
+  
+  // console.log(spinalCase("thisIsSpinalTap"))
+
+  // ________________________________________________
+
+  // function translatePigLatin(str) {
+  //   const vowels = ["a", "e", "i", "o", "u"]
+  //   const splitedStr = str.split('')
+  //   const isVowelsInString = vowels.some(letter => splitedStr.includes(letter))
+
+  //   if (!isVowelsInString) {
+  //     return str + "ay"
+  //   }
+
+  //   if (!vowels.includes(splitedStr[0])) {
+  //     for (let i = 0; i < splitedStr.length; i++) {
+  //       if (!vowels.includes(splitedStr[i])){
+  //         splitedStr.push(splitedStr[i])
+  //         splitedStr.shift(splitedStr[i])
+  //         i--
+  //       } else if (vowels.includes(splitedStr[i])){
+  //         return splitedStr.join('') + "ay"
+  //       }
+  //     }
+      
+  //   } else if (vowels.includes(splitedStr[0])) {
+  //     return str + "way"
+  //   }
+  // }
+  
+  // console.log(translatePigLatin("rhythm"))
+
+ // ________________________________________________
+
+  // function myReplace(str, before, after) {
+  //   const splitedStr = str.split('')
+  //   const idx = str.indexOf(before)
+
+  //   if (before[0].toLowerCase() === before[0]) {
+  //     const afterArr = after.toLowerCase().split('')
+  //     splitedStr.splice(idx, before.length, ...afterArr)
+  //     return splitedStr.join('')
+  //   }
+  //   const upperLetterAfter = after[0].toUpperCase() + after.slice(1)
+  //     const afterArr = upperLetterAfter.split('')
+  //     splitedStr.splice(idx, before.length, ...afterArr)
+  //     return splitedStr.join('')
+  // }
+  
+  // console.log(myReplace("I think we should look up there", "up", "Down"))
+
+// ________________________________________________
+
+  // function pairElement(str) {
+  //   const splitedStr = str.split('')
+  //   const newArr = splitedStr.reduce((acc,item) => {
+  //  switch (item) {
+  //    case "C":
+  //     acc.push(["C", "G"])
+  //    return acc
+  //    case "G":
+  //     acc.push(["G", "C"])
+  //     return acc
+  //    case "A":
+  //     acc.push(["A", "T"])
+  //     return acc
+  //    case "T":
+  //     acc.push(["T", "A"])
+  //     return acc
+  //  }
+  //  }, [])
+  //   return newArr;
+  // }
+  
+  // console.log(pairElement("GCGA"))
+
+// ________________________________________________
+
+  function fearNotLetter(str) {
+    return str;
+  }
+  
+  fearNotLetter("abce");
+ 
